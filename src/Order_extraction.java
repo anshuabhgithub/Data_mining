aec69347378ce4997e9cfb866882c0229b020c5d
anshuabh <anshuabhishekiitk@gmail.com>
2017-04-20 23:07:29 +0530

    final commit

--- src/Order_extraction.java
+++ src/Order_extraction.java
@@ -7,26 +7,26 @@ public class Order_extraction {
 
 public  static void main(String[] args) throws IOException{
 
-	Conversation conv = new Conversation();
+	
 	FileInputStream in_stream = null;
+	for(int i =2;i<46;i++)
+	{
+		Conversation conv = new Conversation();
+		System.out.print("Sanitized"+i+".txt");
 	try{
-	in_stream= new FileInputStream("input.txt");
+	in_stream= new FileInputStream("Sanitized2.txt");
 	InputStreamReader reader = new InputStreamReader(in_stream);
 	BufferedReader rd= new BufferedReader(reader);
 	conv.read_data(rd);
 	conv.create_reg_exp();
-	//conv.create_order(14);
 	conv.data_clean();
 	conv.read_conversation();
-	
-	//conv.check();
-	
-	//conv.print_data();
 	}
 	finally{
 		if(in_stream != null)
 			in_stream.close();
 		}
+	}
 }
 	
 }
